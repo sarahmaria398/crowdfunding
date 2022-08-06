e63afb29a0ada883991a2e14c7130ad29e4d34d7
Sarah <sarahdavis398@gmail.com>
2022-08-06 11:50:11 +1000

    Creating Nav, and env

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
+import HomePage from './pages/HomePage';
+import ProjectPage from './pages/ProjectPage';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Router>
+      <div >
+        <Nav />
+        <Routes >
+          <Route exact path="/" element={<HomePage />}>
+          </Route>
+          <Route exact path="/project" element={<ProjectPage />}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,96 @@
+export const allProjects = [
+    {
+        id: 1,
+        title: "Project One",
+        description: "The first project.",
+        goal: 150,
+        image: "https://imgix.theurbanlist.com/content/article/Brisbane_Cakes_wild_canary.jpg",
+        is_open: false,
+        date_created: "2020-03-20T14:22:23.382748Z",
+        owner: 1
+    },
+    {
+        id: 2,
+        title: "Project Two",
+        description: "The second project.",
+        goal: 150,
+        image: "https://images.immediate.co.uk/production/volatile/sites/30/2020/04/strawberry-cake-8c9a6b6.jpg",
+        is_open: false,
+        date_created: "2020-03-20T14:22:23.382748Z",
+        owner: 1
+    },
+    {
+        id: 3,
+        title: "Project Three",
+        description: "The third project.",
+        goal: 150, image: "https://via.placeholder.com/300.jpg",
+        is_open: false,
+        date_created: "2020-02-20T14:28:23.382748Z",
+        owner: 1
+    },
+    {
+        id: 4,
+        title: "Project Three",
+        description: "The third project.",
+        goal: 150, image: "https://via.placeholder.com/300.jpg",
+        is_open: false,
+        date_created: "2020-02-20T14:28:23.382748Z",
+        owner: 1
+    },
+    {
+        id: 5,
+        title: "Project Three",
+        description: "The third project.",
+        goal: 150, image: "https://via.placeholder.com/300.jpg",
+        is_open: false,
+        date_created: "2020-02-20T14:28:23.382748Z",
+        owner: 1
+    },
+    {
+        id: 6,
+        title: "Project Three",
+        description: "The third project.",
+        goal: 150, image: "https://via.placeholder.com/300.jpg",
+        is_open: false,
+        date_created: "2020-02-20T14:28:23.382748Z",
+        owner: 1
+    },
+]
+
+export const oneProject = {
+    id: 1,
+    title: "Project One",
+    description: "The first project.",
+    goal: 150,
+    image: "https://via.placeholder.com/300.jpg",
+    is_open: false,
+    date_created: "2020-03-20T14:22:23.382748Z",
+    owner: 1,
+    pledges: [{
+        id: 1,
+        amount: 100,
+        comment: "A comment for the pledge",
+        anonymous: false,
+        supporter: 3,
+        project_id: 1,
+    }]
+};
+
+export const twoProject = {
+    id: 1,
+    title: "Project Two",
+    description: "The first project.",
+    goal: 150,
+    image: "https://via.placeholder.com/300.jpg",
+    is_open: false,
+    date_created: "2020-03-20T14:22:23.382748Z",
+    owner: 1,
+    pledges: [{
+        id: 1,
+        amount: 100,
+        comment: "A comment for the pledge",
+        anonymous: false,
+        supporter: 3,
+        project_id: 1,
+    }]
+};
