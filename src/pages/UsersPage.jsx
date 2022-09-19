351486d0ed7a74131c6feb26d944e943c57e29be
Sarah <sarahdavis398@gmail.com>
2022-09-19 22:46:45 +1000

    update users page
    tested sing up and logging in, also correctly unabel to view profile. all wroking.

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -3,8 +3,7 @@ import { useParams, Link } from "react-router-dom";
 
 function UsersPage() {
     const { id } = useParams();
-
-    const [users, updateUsers] = useState([])
+    const [users, updateUsers] = useState([]);
 
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}users/${id}`)
@@ -12,23 +11,26 @@ function UsersPage() {
             .then(data => updateUsers(data))
     }, [])
 
-    return (
-        <div>
-            <div className="user-profile">
-                <img src={users.image} alt="user" />
-                <h3>{users.username}</h3>
-                <h4>{users.bio}</h4>
+    if (window.localStorage.getItem('token')) {
+        return (
+            <div>
+                <div className="user-profile">
+                    <img src={users.image} alt="user" />
+                    <h3>{users.username}</h3>
+                    <h4>{users.bio}</h4>
+                </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
             </div>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-        </div>
-    )
+        )
+    }
+    else { return <div><p>No data for you!</p><br></br><Link className="button" to="/login">Please Log in</Link><br></br></div> }
 }
 
 export default UsersPage;
